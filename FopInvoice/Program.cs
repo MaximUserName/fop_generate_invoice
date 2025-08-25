cdfa4e7f08ee509ba533457a516cbb6a221a4e47
Maxim Ilyukhin <max86work@gmail.com>
2025-08-25 16:26:00 +0300

    initial empty project console

---
+++ FopInvoice/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+static async Task GenerateInvoice()
+{
+    Console.WriteLine("Generating invoice");
+    await Task.CompletedTask;
+}
+
+await GenerateInvoice();
